6dacf7124fbdd4f8c8f152c37c725477caaad3f6
Maksim <maksim.antonov2012@yandex.ru>
2024-12-12 15:46:57 +0300

    Косяки при портировании убрал
    Первый раз горячими клавишами по пользовался

--- Task 1/Task 1.cpp
+++ Task 1/Task 1.cpp
@@ -2,10 +2,10 @@
 #include <vector>
 //task 1 13.1
 int main() {
-	setlocale(lc_all, "russian");
+	setlocale(LC_ALL, "Russian");
 	int n;
-	std::cout << "задание 1\n" << "пользователь вводит с клавиатуры число n, а затем -- n целых чисел.\n" << "необходимо вычислить их среднее арифметическое и вывести на экран.\n";
-	std::cout << "how many elements do you want? ";
+	std::cout << "Задание 1\n" << "Пользователь вводит с клавиатуры число n, а затем -- n целых чисел.\n" << "Необходимо вычислить их среднее арифметическое и вывести на экран.\n";
+	std::cout << "How many elements do you want? ";
 	std::cin >> n;
 	std::vector<int> vec(n);
 	for (int i = 0; i < n;i++) {
