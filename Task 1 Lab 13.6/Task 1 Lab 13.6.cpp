82f7ecb29e0737d08197b08c1c7248023fdf3cca
@MaKc@ @MaKc@ <maksim.antonov2012@yandex.ru>
2024-12-15 21:15:55 +0300

    Первая практическая лаба

---
+++ Task 1 Lab 13.6/Task 1 Lab 13.6.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+int main() {
+	int vecSize, deleteVec;
+	std::cout << "Input vector size: ";
+	std::cin >> vecSize;
+	std::vector<int> vec(vecSize);
+	std::cout << "\n";
+	std::cout << "Input numbers: ";
+	for (int i = 0; i < vec.size(); i++) {
+		std::cin >> vec[i];
+	}
+	std::cout << "\n";
+	std::cout << "Input number to delete: ";
+	std::cin >> deleteVec;
+	for (int i = 0; i < vec.size(); i++) {
+		if (vec[i] == deleteVec) {
+			vec.erase(vec.begin() + i);
+			i--;
+		}
+	}
+	std::cout << "\n";
+	std::cout << "Result: ";
+	for (int i = 0; i < vec.size();i++) {
+		std::cout<< " " << vec[i] << " ";
+	}
+}
