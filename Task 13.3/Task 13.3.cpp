23bdc32e399f7efc69020adf6b6e3b2a5ce37055
@MaKc@ @MaKc@ <maksim.antonov2012@yandex.ru>
2024-12-16 14:59:13 +0300

    Фикс остальных задач

---
+++ Task 13.3/Task 13.3.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <vector>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int quantityRobot;
+	std::cout << "Введите N: ";
+	std::cin >> quantityRobot;
+	std::vector<int> vecRobot(quantityRobot); //Добавил вектор в который помещается количество роботов
+	
+	//Ввод номеров роботов
+	for (int i = 0; i < vecRobot.size(); i++) {
+		std::cout << "Введите номер робота " << i + 1 << ": ";
+		std::cin >> vecRobot[i];
+	}
+	
+	//Граница блока по выводу инфы о роботах
+	std::cout << "На витрине стоят роботы {";
+	for (int i = 0; i < vecRobot.size(); i++) {
+		if (i == vecRobot.size() - 1) {
+			std::cout << vecRobot[i];
+			continue;
+		}
+		std::cout << vecRobot[i] << ",";
+	}
+	std::cout << "}.";
+	//Граница блока по выводу инфы о роботах
+	
+	std::cout << "\n" << std::endl; //Разделяет блок
+	
+	//Блок по покупке
+	int purchaseRobot, numberM; // Переменные отвечающие за покупку и число роботов которые хочет купить клиент (соответсвенно)
+	bool final = true,chek = true;
+	while (final) {
+		chek == true? std::cout << "Введите число M: ": std::cout << "Попробуйте ввести ещё раз количество роботов которое вы хотите купить: "; chek = true;
+		std::cin >> numberM;
+		if (numberM<0 || numberM > quantityRobot) {
+			chek = false;
+			continue;
+		}final = false;
+	}
+	for (int i = 0; i != numberM;++i) {
+		std::cout << "\n";
+		std::cout << "Введите индекс робота для покупки: ";
+		std::cin >> purchaseRobot;
+		if (purchaseRobot > quantityRobot || purchaseRobot < 0) {
+			std::cout << "Такого индекса нет\n";
+			--i;
+			continue;
+		}else	{
+			vecRobot.erase(vecRobot.begin() + purchaseRobot);
+			std::cout << "Забрали робота с индексом " << purchaseRobot;
+		}
+		std::cout << "\n";
+		std::cout << "На витрине остались роботы {";
+		for (int i = 0; i < vecRobot.size(); i++) {
+			if (i == vecRobot.size() - 1) {
+				std::cout << vecRobot[i]; 
+				continue;
+			}
+			std::cout << vecRobot[i] << ",";
+		}
+		std::cout << "}.";
+	}
+	//Блок по покупке
+}
---
+++ Task 2 Lab 13.6/Task 2 Lab 13.6.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+#include <vector>
+
+int main() {
+	//Задал вектор цен на продукты
+	std::vector<float> prices{ 2.5, 4.25, 3.0, 10.0 };
+	//Задал вектор покупок
+	std::vector<int> items{ 1, 1, 0, 3 };
+
+	//Переменная итоговой стоимости
+	float totalCost = 0.0;
+	
+	//По совету делаю один цикл для вычисления общей стоимости
+	for (int index : items) {
+		if (index>=0&&index<prices.size()) {
+			totalCost += prices[index];
+		}else {
+			std::cerr << "Ошибка: индекс " << index << " выходит за границы вектора цен.\n";
+			return 1;
+		}
+	}
+
+	std::cout << "Итоговая стоимость покупок: " << totalCost << std::endl;
+
+	return 0;
+}
